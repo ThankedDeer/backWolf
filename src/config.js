e8b4c42060ca0d03ec75e5d2801db2173051811f
ThankedDeer <117323510+ThankedDeer@users.noreply.github.com>
2023-03-02 03:24:40 -0600

    primer commit

--- src/config.js
+++ src/config.js
@@ -2,10 +2,9 @@ import { config } from "dotenv";
 
 config()
 
-export const PORT = process.env.PORT
-export const DB_USER = process.env.DB_USER || "root"
-export const DB_PASSWORD = process.env.DB_PASSWORD || "uUf3PbIQ2Ha50aZwtIE5"
-export const DB_HOST = process.env.DB_HOST || "localhost"
-export const DB_PORT = process.env.DB_PORT || 7827 
-
-export const DB_DATABASE = process.env.DB_DATABASE
+export const PORT = process.env.PORT 
+export const DB_USER = process.env.DB_USER 
+export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DB_HOST = process.env.DB_HOST 
+export const DB_PORT = process.env.DB_PORT  
+export const DB_NAME = process.env.DB_NAME
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import app from "./app.js";
+import  express  from 'express'
+import numeros from './routes/numeros.routes.js'
+import reportes from './routes/reportes.routes.js'
+import cors from 'cors'
 import {PORT} from'./config.js'
+
+
+const app = express()
+app.use(cors());
+
+app.use(express.json())
+
+app.use(reportes)
+app.use(numeros)
 
 
 app.listen(PORT)
